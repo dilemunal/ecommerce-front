fab8eee5063018dc9b86d48041a0b9a06d9e53d5
Tolgahan İnan <tolgahaninan34@gmail.com>
2021-12-13 05:19:49 +0300

    Main Page
    general layout of main page

---
+++ Home/homepage.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+ 
+    $("#homepageSliderWrapper").owlCarousel({
+   
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        items:1,
+        margin:50,
+        autoplay:true,
+		autoplayHoverPause:true,
+        loop:true,
+   
+    });
+   
+  });
